65cc9b9c0d169ff8cc171dbca8ba2f39f5a1b586
Cype Core <akoval@cypecore.com>
2025-04-19 23:11:22 +1000

    Implement testWait function with argument handling and error logging

--- modern/test.ts
+++ modern/test.ts
@@ -1,3 +1,23 @@
-const a = ()=>{
-  console.log('hello world');
+const testWait = async () => {
+  const args = process.argv.slice(2); 
+  console.dir(args, { depth: null });
+  
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  console.log("hello world");
+};
+
+const toRun = {
+  testWait,
 }
+const run = async () => {
+  const args = process.argv.slice(2);
+  const k = args[0];
+  const f = toRun[k];  
+  if (f) {
+    await f();
+  }
+};
+run().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
